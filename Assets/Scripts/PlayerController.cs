25ab99e1af1e663d8fe2308fdd2c015c06eaa698
tranglq <lequynhtrang120296@gmail.com>
2017-10-06 17:33:05 +0700

    playercontroller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+
+    private Rigidbody rb;
+
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveVertical*speed, moveHorizontal*speed, 0.0f);
+        rb.rotation = Quaternion.Euler(movement);
+	}
+}
